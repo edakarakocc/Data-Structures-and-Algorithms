3d18eeeea8a3b514efc1a5a478259b2d41320719
Eda <160626133+edakarakocc@users.noreply.github.com>
2024-11-23 20:02:42 +0300

    Update Majority_Element.cs
    Documented the time complexity of the existing implementation as a comment in the code. Additionally, provided an alternative implementation with improved time complexity for comparison.

--- Majority_Element.cs
+++ Majority_Element.cs
@@ -1,4 +1,4 @@
-﻿// O(n^2) / O(1)
+// O(n^2) / O(1)
 internal class Program
 {
     private static void Main(string[] args)
@@ -28,3 +28,59 @@ internal class Program
     }
 }
 
+
+
+/* O(n) / O(n)
+ * 
+ * internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Dictionary<int, int> nums = new Dictionary<int, int> { { 1, 1 }, { 5, 1 }, { 2, 6 }, { 3, 3 } }; // {key,value} ; the key represents the number, and the value represents its quantity.
+        int max_value = 0;
+        var majority = 0;
+
+        foreach(var key in nums)
+        {
+            if (key.Value > max_value)
+            {
+                max_value = key.Value;
+                majority = key.Key;
+            }
+        }
+        Console.WriteLine(majority);
+        Console.ReadKey();
+    }
+}*/
+
+
+
+
+/* O(n) / O(n)  This shows a different approach to using a dictionary.
+ * 
+ * internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> nums = new List<int> { 1, 1, 2, 3, 3, 5, 5, 5, 5, 5, 5 };
+        Dictionary<int, int> numbers = new Dictionary<int, int> {};
+        int max_repeat = 0;
+        int maxValue = 0;
+
+        foreach(int num in nums)
+        {
+            if (numbers.ContainsKey(num))
+                numbers[num] += 1;
+            else
+                numbers[num] = 1;
+
+            if (numbers[num] > max_repeat)
+            {
+                max_repeat = numbers[num];
+                maxValue = num;
+            }
+        }
+        Console.WriteLine(maxValue);
+        Console.ReadKey();
+    }
+}*/
