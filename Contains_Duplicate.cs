d1321545e5f8ff34558994a55d0e6c805484b138
Eda <160626133+edakarakocc@users.noreply.github.com>
2024-11-23 16:58:01 +0300

    Update Contains_Duplicate.cs
    Documented the time complexity of the existing implementation as a comment in the code. Additionally, provided an alternative implementation with improved time complexity for comparison.

--- Contains_Duplicate.cs
+++ Contains_Duplicate.cs
@@ -1,4 +1,5 @@
-﻿internal class Program
+// O(n^2)
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -20,3 +21,33 @@
         Console.ReadKey();
     }
 }
+
+
+
+/* O(n)
+ * using System;
+using System.Collections.Generic;
+
+namespace Data_Structures_and_Algorithms
+{
+    internal class Contains_Duplicate
+    {
+        private static void Main(string[] args)
+        {
+            List<int> nums = new List<int> { 1, 2, 3, 4, 5, 1, 2 };
+            HashSet<int> uniqueNumbers = new HashSet<int>();
+            bool hasDuplicated = false;
+
+            foreach (int num in nums)
+            {
+                if (!uniqueNumbers.Add(num))
+                {
+                    hasDuplicated = true;
+                    break;
+                }
+            }
+            Console.WriteLine(hasDuplicated);
+            Console.ReadKey();
+        }
+    }
+}*/
