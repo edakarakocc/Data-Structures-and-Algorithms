1a1cef7b5f428ef701cf446ee9fff8aa80c24863
Eda <160626133+edakarakocc@users.noreply.github.com>
2024-11-23 18:20:26 +0300

    Add Single_Number.cs

---
+++ Single_Number.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> nums = new List<int> { 2, 2, 5, 3, 4, 4, 3, 5, 9 };
+
+        for(int i = 0; i < nums.Count; i++)
+        {
+            int count = 0;
+
+            for(int j = 0; j < nums.Count; j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    count++;
+                }
+
+            }
+
+            if (count == 1)
+            {
+                Console.WriteLine(nums[i]);
+                break;
+            }
+        }
+
+        Console.ReadKey();
+
+    }
+}
